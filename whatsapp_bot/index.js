d5cfc274f6bfe789b8e03e3bd4d58db873159b7a
hassanamin994 <hassanmamin994@gmail.com>
2020-05-23 05:00:09 +0200

    add whatsapp bot workers

---
+++ whatsapp_bot/index.js
@@ -0,0 +1,22 @@
+
+const rabbitmqService = require('../vendors/rabbitmq');
+const queues = require('../vendors/rabbitmq/queues');
+
+module.exports = (RABBITMQ_SERVER) => {
+
+    let rabbitmqChannel;
+    rabbitmqService.createChannel(RABBITMQ_SERVER, (err, channel) => {
+        if (err) {
+            throw err;
+        }
+        rabbitmqChannel = channel;
+    });
+
+    function whatsappNotifyUserVideoProofreadingReady({ videoId }) {
+        return rabbitmqChannel.sendToQueue(queues.WHATSAPP_NOTIFY_USER_VIDEO_PROOFREADING_READY, new Buffer(JSON.stringify(params)), { persistent: true });
+    }
+    
+    return {
+       whatsappNotifyUserVideoProofreadingReady 
+    }
+}
