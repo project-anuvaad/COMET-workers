cc83db9f7b27adcc9750a4b96d9db0665d4622f7
hassanamin994 <hassanmamin994@gmail.com>
2020-06-15 03:54:19 +0200

    add updateArticleSlideVideoSpeed

--- exporter/index.js
+++ exporter/index.js
@@ -11,41 +11,45 @@ module.exports = RABBITMQ_SERVER => {
         }
         rabbitmqChannel = channel;
     });
-    
-    
+
+
     function convertVideoToArticle({ videoId, articleId }) {
         return rabbitmqChannel.sendToQueue(queues.CONVERT_VIDEO_TO_ARTICLE_QUEUE, new Buffer(JSON.stringify({ videoId, articleId })), { persistent: true });
     }
-    
+
     function exportArticleTranslation(translationExportId) {
         return rabbitmqChannel.sendToQueue(queues.EXPORT_ARTICLE_TRANSLATION, new Buffer(JSON.stringify({ translationExportId })), { persistent: true });
     }
-    
+
     function archiveArticleTranslationAudios(translationExportId) {
         return rabbitmqChannel.sendToQueue(queues.ARCHIVE_ARTICLE_TRANSLATION_AUDIOS, new Buffer(JSON.stringify({ translationExportId })), { persistent: true });
     }
-    
+
     function burnTranslatedArticleVideoSubtitle(translationExportId) {
         return rabbitmqChannel.sendToQueue(queues.BURN_ARTICLE_TRANSLATION_VIDEO_SUBTITLE, new Buffer(JSON.stringify({ translationExportId })), { persistent: true });
     }
-    
+
     function generateTranslatedArticleSubtitles(translationExportId) {
         return rabbitmqChannel.sendToQueue(queues.GENERATE_ARTICLE_TRANSLATION_VIDEO_SUBTITLE, new Buffer(JSON.stringify({ translationExportId })), { persistent: true });
     }
-    
+
     function generateVideoThumbnail(videoId) {
         return rabbitmqChannel.sendToQueue(queues.GENERATE_VIDEO_THUMBNAIL_QUEUE, new Buffer(JSON.stringify({ videoId })), { persistent: true });
     }
-    
+
     function updateArticleVideoSpeed({ articleId, videoSpeed }) {
         return rabbitmqChannel.sendToQueue(queues.UPDATE_ARTICLE_VIDEO_SPEED, new Buffer(JSON.stringify({ articleId, videoSpeed })), { persistent: true });
+    }
+
+    function updateArticleSlideVideoSpeed({ articleId, videoSpeed, slidePosition, subslidePosition }) {
+        return rabbitmqChannel.sendToQueue(queues.UPDATE_ARTICLE_VIDEO_SPEED, new Buffer(JSON.stringify({ articleId, videoSpeed, slidePosition, subslidePosition })), { persistent: true });
     }
 
     function burnTranslatedArticleVideoSubtitleAndSignlanguage(translationExportId) {
         return rabbitmqChannel.sendToQueue(queues.BURN_ARTICLE_TRANSLATION_VIDEO_SUBTITLE_AND_SIGNLANGUAGE, new Buffer(JSON.stringify({ translationExportId })), { persistent: true });
     }
-    
-    
+
+
     return {
         convertVideoToArticle,
         exportArticleTranslation,
@@ -54,6 +58,7 @@ module.exports = RABBITMQ_SERVER => {
         generateTranslatedArticleSubtitles,
         generateVideoThumbnail,
         updateArticleVideoSpeed,
+        updateArticleSlideVideoSpeed,
         burnTranslatedArticleVideoSubtitleAndSignlanguage,
     }
 
