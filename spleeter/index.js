5132089b8c3efd18c3b60a245a28c3659608a3df
Osama Hamed <osamahamedmohammed@gmail.com>
2020-07-31 05:34:46 +0200

    extract video voice

--- spleeter/index.js
+++ spleeter/index.js
@@ -8,7 +8,12 @@ module.exports = ({ rabbitmqChannel }) => {
         return rabbitmqChannel.sendToQueue(queues.EXTRACT_VIDEO_BACKGROUND_MUSIC_QUEUE, new Buffer(JSON.stringify({ id, url })), { persistent: true });
     }
     
+    function extractVideoVoice({ id, url }) {
+        return rabbitmqChannel.sendToQueue(queues.EXTRACT_VIDEO_VOICE_QUEUE, new Buffer(JSON.stringify({ id, url })), { persistent: true });
+    }
+
     return {
-        extractVideoBackgroundMusic
+        extractVideoBackgroundMusic,
+        extractVideoVoice
     }
 }
